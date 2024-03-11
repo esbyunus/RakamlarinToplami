e03573f0e5729275614f7cb46bd945b436e8f3e9
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-03-11 22:12:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace justmath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rakamlarinin toplamini istediginiz sayiyi giriniz: ");
+            int n = Convert.ToInt32 (Console.ReadLine());
+            Console.WriteLine($"{n} sayinin rakamlari toplami = {math.RakamToplam(n)}");
+        }
+    }
+}
---
+++ math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace justmath
+{
+    internal class math
+    {
+        /// <summary>
+        /// Parametere olarak aldigi sayinin rakamlari toplamini doner
+        /// </summary>
+        /// <param name="n">sayi</param>
+        /// <returns>rakamlarin toplami</returns>
+        public static int RakamToplam(int n)
+        {
+            int toplam = 0, rakam = 0;
+            while (n>0)
+            {
+                rakam = n % 10; // n'nin 10 modunu alır. Yani rakamın 10 ile bolumunun kalani alinir.
+                toplam += rakam; // rakam, toplama aktarılır
+                n = n / 10; // n 10'a bolunur.
+            }
+            return toplam;
+        }
+    }
+}
